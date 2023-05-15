13065e63bc44e3be7dd98f02ec56993ef4621ef8
RDW <duckwhale@mail.ru>
2023-05-15 19:52:40 +0200

    Client: Let the WebView load resources instead of the runtime
    The original approach causes the CSS to not be requested by the WebView, which would shift the burden of resource loading to the runtime. Needless to say, that is neither desirable nor necessary.

--- Core/WebClient.lua
+++ Core/WebClient.lua
@@ -3,9 +3,9 @@ local WebClient = {}
 function WebClient:Start()
 	print("Creating BJS demo scene in a native WebView window ...")
 	C_WebView.CreateWithDevTools()
-	local htmlEntryPoint = C_FileSystem.ReadFile("Core/WebClient/index.html")
-	C_WebView.SetHTML(htmlEntryPoint)
-	C_WebView.SetWindowTitle("WebView Demo Scene")
+	local htmlEntryPoint = "http://localhost:9005/Core/WebClient/index.html"
+	C_WebView.NavigateToURL(htmlEntryPoint) -- Loading the HTML directly won't resolve external files (e.g., CSS)
+	C_WebView.SetWindowTitle("RagLite WebClient")
 	C_WebView.ToggleFullscreenMode()
 end
 
