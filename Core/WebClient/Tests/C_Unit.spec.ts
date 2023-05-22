134f1b4df34ece423e9c689b4f7d5b39955e8934
RDW <duckwhale@mail.ru>
2023-05-23 01:33:10 +0200

    Update C_Unit.spec.ts

--- Core/WebClient/Tests/C_Unit.spec.ts
+++ Core/WebClient/Tests/C_Unit.spec.ts
@@ -1,13 +1,19 @@
 import { deepStrictEqual as assertEquals } from "node:assert";
 
+import { Vector2 as Vector2D } from "@babylonjs/core";
+
 import C_Unit from "../API/WorldState/C_Unit";
 
-describe('API: WorldState', () => {
-describe('Namespace: C_Unit', () => {
-describe('Function: getWorldPosition', () => {
-  it('should return null if an unknown unit ID was passed', () => {
-    assertEquals(C_Unit.getWorldPosition("does-not-exist"), null);
-  });
-  });
+describe("WorldState", () => {
+  describe("C_Unit", () => {
+    describe("getWorldPosition", () => {
+      it("should return null if an unknown unit ID was passed", () => {
+        assertEquals(C_Unit.getWorldPosition("does-not-exist"), null);
+      });
+
+      it("should return a position vector if the unit ID is known", () => {
+        assertEquals(C_Unit.getWorldPosition("player"), new Vector2D(0, 0));
+      });
+    });
   });
 });
