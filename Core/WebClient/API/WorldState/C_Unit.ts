ccafd72701ffedc5cf4755a80d9503898ab7966c
RDW <duckwhale@mail.ru>
2023-05-23 18:21:59 +0200

    Update C_Unit.ts

--- Core/WebClient/API/WorldState/C_Unit.ts
+++ Core/WebClient/API/WorldState/C_Unit.ts
@@ -3,6 +3,10 @@ import { Vector2 as Vector2D } from "@babylonjs/core";
 class Unit {
   unitID : string;
   mapPosition: Vector2D;
+  constructor(unitID : string) {
+	this.unitID = unitID;
+	this.mapPosition = new Vector2D(0, 0);
+  }
   // worldPosition: Vector3D;
 }
 
@@ -15,6 +19,17 @@ const C_Unit = {
 
 	return new Vector2D(0, 0); // TODO replace with actual unit position
   },
+  addKnownUnit(unitID : string, unitToAdd : Unit) {
+	// NYI
+  },
+  removeKnownUnit(unitID : string) {
+	// NYI
+  },
+  setMapPosition(unitID : string, newMapPosition : Vector2D) {
+	// NYI
+  }
 } as const;
 
 export default C_Unit;
+
+export {Unit};
