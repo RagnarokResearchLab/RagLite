0419e536e5434b1a5f0d3ee5dcea225a6c01ee4a
RDW <duckwhale@mail.ru>
2023-05-23 11:29:41 +0200

    Update C_Unit.ts

--- Core/WebClient/API/WorldState/C_Unit.ts
+++ Core/WebClient/API/WorldState/C_Unit.ts
@@ -1,6 +1,19 @@
+import { Vector2 as Vector2D } from "@babylonjs/core";
+
+class Unit {
+  unitID : string;
+  mapPosition: Vector2D;
+  // worldPosition: Vector3D;
+}
+
 const C_Unit = {
-  getWorldPosition: (unitID: string) => {
-    return 42;
+  knownUnits: {} as Record<string, Unit>,
+  getWorldPosition: (unitID: string): Vector2D | null => {
+	const unit = C_Unit.knownUnits[unitID];
+
+	if(!unit) return null;
+
+	return new Vector2D(0, 0); // TODO replace with actual unit position
   },
 } as const;
 
