667918c3c77743900bbcd69c25e670332ee4ab60
RDW <duckwhale@mail.ru>
2023-05-25 22:04:02 +0200

    remove minimap transparent pixels (wasteful, do on server?)

--- Core/WebClient/Interface/Overlays/MiniMapOverlay.tsx
+++ Core/WebClient/Interface/Overlays/MiniMapOverlay.tsx
@@ -1,12 +1,12 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useRef, useEffect } from "react";
 import SharedWorldStateContext from "../SharedWorldStateContext";
 import SharedDatabaseContext from "../SharedDatabaseContext";
-
 import GameTooltip from "../Tooltips/GameTooltip";
 
 const placeholderMiniMapImage = "Interface/Assets/minimap-placeholder.bmp";
 
 const MiniMap = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const [isHovering, setIsHovering] = useState(false);
   const worldState = useContext(SharedWorldStateContext);
   const db = useContext(SharedDatabaseContext);
@@ -14,6 +14,33 @@ const MiniMap = () => {
   if (!worldState || !db) {
     return <div>Loading...</div>;
   }
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      const img = new Image();
+      img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx?.drawImage(img, 0, 0, img.width, img.height);
+
+        const imageData = ctx?.getImageData(0, 0, img.width, img.height);
+        if (imageData) {
+          const data = imageData.data;
+          for (let i = 0; i < data.length; i += 4) {
+            if (data[i] === 255 && data[i + 1] === 0 && data[i + 2] === 255) {
+              data[i + 3] = 0;
+            }
+          }
+          ctx?.putImageData(imageData, 0, 0);
+        }
+      };
+      img.src = "http://localhost:9005/ui/minimap/" + worldState.mapID + ".bmp";
+    }
+  }, [worldState.mapID]);
+
+
 
   const displayName = db[worldState.mapID] || worldState.mapID;
   const isInLoginScreen = worldState.mapID === "login_screen";
@@ -32,11 +59,7 @@ const MiniMap = () => {
       >
         {displayName}
       </p>
-      <img
-        src={miniMapImageToDisplay}
-        className="minimap-image"
-        alt={displayName}
-      />
+      <canvas ref={canvasRef} className="minimap-image" />
       {isHovering && <GameTooltip>{displayName}</GameTooltip>}
     </div>
   );
