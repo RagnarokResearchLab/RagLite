6fe811ddac92b5d68bafa28dc7c527a6ac7755f4
RDW <duckwhale@mail.ru>
2023-07-06 19:56:44 +0200

    Tests: Add a unit test for the GND lightmap exporter

--- Tests/Tools/RagnarokTools.spec.lua
+++ Tests/Tools/RagnarokTools.spec.lua
@@ -1,4 +1,5 @@
 local json = require("json")
+local openssl = require("openssl")
 
 local RagnarokTools = require("Tools.RagnarokTools")
 
@@ -39,6 +40,28 @@ describe("RagnarokTools", function()
 
 			local expectedJSON = json.prettier(mapList)
 			assertEquals(storedJSON, expectedJSON)
+		end)
+	end)
+
+	describe("ExportLightmapsFromGND", function()
+		it("should export the GND lightmap data in a human-readable format if a valid GND buffer is passed", function()
+			local expectedLightmapChecksum = "be735bab85771a54892d4129d7aba3126e0f7f41f2c9891a28aa8dcfc897d2fa"
+			local expectedShadowmapChecksum = "4a7a36bedbb8e73797b91b2f568b59b8d4600dc3975e2265114339a5142e9175"
+
+			local gndFileContents = C_FileSystem.ReadFile(path.join("Tests", "Fixtures", "no-water-plane.gnd"))
+			RagnarokTools:ExportLightmapsFromGND(gndFileContents)
+
+			local generatedLightmapBytes = C_FileSystem.ReadFile("lightmap.png")
+			local generatedShadowmapBytes = C_FileSystem.ReadFile("shadowmap.png")
+
+			local generatedLightmapChecksum = openssl.digest.digest("sha256", generatedLightmapBytes)
+			local generatedShadowmapChecksum = openssl.digest.digest("sha256", generatedShadowmapBytes)
+
+			assertEquals(generatedLightmapChecksum, expectedLightmapChecksum)
+			assertEquals(generatedShadowmapChecksum, expectedShadowmapChecksum)
+
+			C_FileSystem.Delete("lightmap.png")
+			C_FileSystem.Delete("shadowmap.png")
 		end)
 	end)
 end)
