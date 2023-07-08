d6aaf5e0db58c9134b068bbeb3a34041bf6ca9b7
RDW <duckwhale@mail.ru>
2023-07-08 13:23:57 +0200

    Client: Add an API namespace for cursor position queries
    This doesn't really belong in the event handling code.

--- Tests/unit-test.lua
+++ Tests/unit-test.lua
@@ -5,6 +5,7 @@ local specFiles = {
 	"Tests/FileFormats/RagnarokGRF.spec.lua",
 	"Tests/AssetServer/AssetServer.spec.lua",
 	"Tests/NativeClient/C_Camera.spec.lua",
+	"Tests/NativeClient/C_Cursor.spec.lua",
 	"Tests/NativeClient/Matrix3D.spec.lua",
 	"Tests/NativeClient/Matrix4D.spec.lua",
 	"Tests/NativeClient/Vector3D.spec.lua",
