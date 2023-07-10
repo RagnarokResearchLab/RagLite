224f6cd3094969784cf4e0d4a0cd6049385f0896
RDW <duckwhale@mail.ru>
2023-07-10 18:13:49 +0200

    Update dump-spr-images.lua

--- Tools/dump-spr-images.lua
+++ Tools/dump-spr-images.lua
@@ -12,7 +12,7 @@ local spr = RagnarokSPR()
 spr:DecodeFileContents(sprFileContents)
 
 local palette = spr:GetEmbeddedColorPalette(sprFileContents)
-C_FileSystem.WriteFile("palette.bin", tostring(buffer.new(1024):putcdata(palette, 1024)))
+C_FileSystem.WriteFile("spr-export/palette.bin", tostring(buffer.new(1024):putcdata(palette, 1024)))
 
 for index=0, spr.bmpImagesCount - 1, 1 do
 -- for index=0, 1, 1 do
@@ -21,21 +21,24 @@ for index=0, spr.bmpImagesCount - 1, 1 do
 	local rgbaImageBytes = spr:ApplyColorPalette(indexedColorImageBytes, palette)
 	C_FileSystem.WriteFile("spr-export/rgba-frame-" .. index .. ".bin", tostring(rgbaImageBytes))
 
+	assert(#rgbaImageBytes == spr.bmpImages[index].pixelWidth * spr.bmpImages[index].pixelHeight * 4, "WTF?")
+
 	-- TODO high-level API for this, it's a PITA
 	local image = ffi.new("stbi_image_t")
 	image.width = spr.bmpImages[index].pixelWidth
 	image.height = spr.bmpImages[index].pixelHeight
-	image.data = rgbaImageBytes
+	image.data = rgbaImageBytes:ref()
 	image.channels = 4
 
-	local maxFileSize = stbi.max_bitmap_size(image.width, image.height, image.channels)
+	-- local maxFileSize = stbi.max_bitmap_size(image.width, image.height, image.channels)
+	local maxFileSize = stbi.bindings.stbi_get_required_bmp_size(image)
 	local outputBuffer = buffer.new()
 	local startPointer, length = outputBuffer:reserve(maxFileSize)
 
-	-- print(length, #rgbaImageBytes)
 
 	local numBytesWritten = stbi.bindings.stbi_encode_bmp(image, startPointer, length)
-
+	print(tonumber(length), tonumber(numBytesWritten), tonumber(maxFileSize))
+	assert(numBytesWritten <= maxFileSize, "Too many bytes written?")
 	outputBuffer:commit(numBytesWritten)
 
 	C_FileSystem.WriteFile("spr-export/rgba-frame-" .. index .. ".bmp", tostring(outputBuffer))
