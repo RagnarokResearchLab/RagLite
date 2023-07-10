04d1fcbdac12279ed663a99fa1e1e3dddc395808
RDW <duckwhale@mail.ru>
2023-07-10 18:14:00 +0200

    revert maybe?

--- Core/FileFormats/RagnarokSPR.lua
+++ Core/FileFormats/RagnarokSPR.lua
@@ -149,21 +149,26 @@ function RagnarokSPR:GetEmbeddedColorPalette(fileContents)
 	end
 
 	local bufferAreaStartPointer = fileContents:ref()
-	local paletteBytes = ffi_cast("uint8_t*", bufferAreaStartPointer + paletteStartOffset)
-	local bmpColorPalette = ffi_cast("spr_palette_t*", paletteBytes)
+	local paletteBytes = ffi_cast("spr_palette_t*", bufferAreaStartPointer + paletteStartOffset)
 
-	return bmpColorPalette
+	-- Must copy to create a GC anchor here before the buffer is collected (probably not a big deal?)
+	local bmpColorPalette = ffi_new("spr_palette_t[1]", paletteBytes[0])
+
+	return bmpColorPalette[0]
 end
 
 function RagnarokSPR:ApplyColorPalette(indexedColorImageBytes, palette)
 	local rgbaImageBytes = buffer.new(#indexedColorImageBytes * 4)
 
 	local startPointer = indexedColorImageBytes:ref()
+
 	local bytes = ffi_cast("uint8_t*", startPointer)
 
 	for byteIndex = 0, #indexedColorImageBytes - 1, 1 do
 		local paletteIndex = bytes[byteIndex]
+		-- print(byteIndex, paletteIndex, palette)
 		local paletteColor = palette.colors[paletteIndex]
+		-- print(paletteColor.red, paletteColor.green, paletteColor.blue, paletteColor.alpha)
 		rgbaImageBytes:putcdata(paletteColor, ffi_sizeof(paletteColor))
 	end
 
