6f22458dc6b0fd4010b0bfd7562c9f5406519146
RDW <duckwhale@mail.ru>
2023-07-10 16:54:27 +0200

    Update dump-spr-images.lua

--- Tools/dump-spr-images.lua
+++ Tools/dump-spr-images.lua
@@ -1,8 +1,24 @@
 local RagnarokSPR = require("Core.FileFormats.RagnarokSPR")
 
-local compressedBuffer = buffer.new(990)
-local frameBytes = C_FileSystem.ReadFile("v2-1-goldporing-rle-encoded-frame0.bin")
-compressedBuffer:put(frameBytes)
-local decompressedBuffer = buffer.new(1332)
+local sprFilePath = "Tests/Fixtures/v2-1.spr"
+local sprFileContents = C_FileSystem.ReadFile(sprFilePath)
 
-RagnarokSPR:DecompressRunLengthEncodedBytes(compressedBuffer, decompressedBuffer)
+local spr = RagnarokSPR()
+spr:DecodeFileContents(sprFileContents)
+
+local palette = spr:GetEmbeddedColorPalette(sprFileContents)
+C_FileSystem.WriteFile("palette.bin", tostring(buffer.new(1024):putcdata(palette, 1024)))
+
+-- for index=0, spr.bmpImagesCount - 1, 1 do
+for index=0, 1, 1 do
+	local indexedColorImageBytes = spr.bmpImages[index].decompressedImageBuffer
+	local rgbaImageBytes = spr:ApplyColorPalette(indexedColorImageBytes, palette)
+	C_FileSystem.WriteFile("rgba-frame-" .. index .. ".bin", tostring(rgbaImageBytes))
+end
+
+-- local compressedBuffer = buffer.new(990)
+-- local frameBytes = C_FileSystem.ReadFile("v2-1-goldporing-rle-encoded-frame0.bin")
+-- compressedBuffer:put(frameBytes)
+-- local decompressedBuffer = buffer.new(1332)
+
+-- RagnarokSPR:DecompressRunLengthEncodedBytes(compressedBuffer, decompressedBuffer)
