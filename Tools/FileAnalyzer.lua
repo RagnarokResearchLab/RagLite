d2e839ef0cd388f38c0ea85d5770639906373bbc
RDW <duckwhale@mail.ru>
2023-08-14 14:41:20 +0200

    Tools: Add support for SPR files to the FileAnalyzer
    Bit of a copy/paste issue here, may have to streamline later.

--- Tools/FileAnalyzer.lua
+++ Tools/FileAnalyzer.lua
@@ -1,4 +1,5 @@
 local RagnarokGND = require("Core.FileFormats.RagnarokGND")
+local RagnarokSPR = require("Core.FileFormats.RagnarokSPR")
 
 local FileAnalyzer = {}
 
@@ -33,6 +34,41 @@ function FileAnalyzer:AnalyzeGND(gndFiles)
 
 		analysisResult.fields.gridSizeV[gnd.gridSizeV] = analysisResult.fields.gridSizeV[gnd.gridSizeV] or 0
 		analysisResult.fields.gridSizeV[gnd.gridSizeV] = analysisResult.fields.gridSizeV[gnd.gridSizeV] + 1
+
+		analysisResult.numFilesAnalyzed = analysisResult.numFilesAnalyzed + 1
+	end
+
+	return analysisResult
+end
+
+function FileAnalyzer:AnalyzeSPR(sprFiles)
+	local analysisResult = {
+		numFilesAnalyzed = 0,
+		fields = {
+			version = {},
+			bmpImagesCount = {},
+			tgaImagesCount = {},
+		},
+	}
+
+	local spr = RagnarokSPR()
+	for index, filePath in ipairs(sprFiles) do
+		printf("Analyzing file: %s", filePath)
+
+		local sprFileContents = C_FileSystem.ReadFile(filePath)
+		spr:DecodeFileContents(sprFileContents)
+
+		analysisResult.fields.version[spr.version] = analysisResult.fields.version[spr.version] or 0
+		analysisResult.fields.version[spr.version] = analysisResult.fields.version[spr.version] + 1
+
+		analysisResult.fields.bmpImagesCount[spr.bmpImagesCount] = analysisResult.fields.bmpImagesCount[spr.bmpImagesCount]
+			or 0
+		analysisResult.fields.tgaImagesCount[spr.tgaImagesCount] = analysisResult.fields.tgaImagesCount[spr.tgaImagesCount]
+			or 0
+		analysisResult.fields.bmpImagesCount[spr.bmpImagesCount] = analysisResult.fields.bmpImagesCount[spr.bmpImagesCount]
+			+ 1
+		analysisResult.fields.tgaImagesCount[spr.tgaImagesCount] = analysisResult.fields.tgaImagesCount[spr.tgaImagesCount]
+			+ 1
 
 		analysisResult.numFilesAnalyzed = analysisResult.numFilesAnalyzed + 1
 	end
