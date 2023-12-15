c97c947d5d440b4ff2db11266906d00a2fd1e0aa
RDW <duckwhale@mail.ru>
2023-12-15 06:26:36 +0100

    Cleanup: Remove the obsolete WebClient prototype
    WebGPU is the future. I'll re-add the relevant bits and pieces later and/or re-implement them as needed... without the React/TypeScript madness or browser limitations.
    
    Leaving  the placeholder mini map asset to be version-controlled, as it can't be backed up via GitHub gists and otherwise I'll forget where I put the thing...

--- main.lua
+++ main.lua
@@ -1,9 +1,7 @@
 package.path = "?.lua"
 
 local AssetServer = require("Core.AssetServer")
-local WebClient = require("Core.WebClient")
 local WorldServer = require("Core.WorldServer")
 
 AssetServer:Start("data.grf")
-WebClient:Start()
 WorldServer:Start()
