9016fe7f0c5a66a53b5cdd63fa2fdcb8be3ffcca
RDW <duckwhale@mail.ru>
2023-12-16 01:13:53 +0100

    Client: Add some shorthands for commonly-used colors
    This is just so I don't forget the hex codes and have to look them up again.

--- Core/NativeClient/DebugDraw/Color.lua
+++ Core/NativeClient/DebugDraw/Color.lua
@@ -6,6 +6,9 @@ local Color = {
 	YELLOW = { red = 1, green = 1, blue = 0 },
 	MAGENTA = { red = 1, green = 0, blue = 1 },
 	CYAN = { red = 0, green = 1, blue = 1 },
+	PINK = { red = 1, green = 0x7F / 255, blue = 1 }, -- Fallback color (diffuse textures)
+	SKY = { red = 0, green = 0x77 / 255, blue = 1 }, -- Clear color (game world)
+	GREY = { red = 0.05, green = 0.05, blue = 0.05 }, -- Clear color (development)
 }
 
 return Color
