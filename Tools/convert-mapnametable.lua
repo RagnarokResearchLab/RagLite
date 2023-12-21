72d3c9d673d8023cc2c40f088d30b2256aed792c
RDW <duckwhale@mail.ru>
2023-12-21 03:37:49 +0100

    Tools: Rework the map DB script to export a Lua table
    The initial table was generated by processing Koko's DB, but now the kRO maps need to be added as well (without discarding the existing entries).

--- Tools/convert-mapnametable.lua
+++ Tools/convert-mapnametable.lua
@@ -1,14 +1,36 @@
-local json = require("json")
+local RagnarokGRF = require("Core.FileFormats.RagnarokGRF")
 
 local string_explode = string.explode
 local string_gsub = string.gsub
 
-local mapNameTable = C_FileSystem.ReadFile("mapnametable.txt") -- Adjust path as needed (or extract from GRF)
+local mode = arg[1] or "grf"
+local source = arg[2]
+
+local mapNameTable
+if mode == "grf" then
+	source = source or "data.grf"
+	local grf = RagnarokGRF()
+	grf:Open(source)
+	mapNameTable = grf:ExtractFileInMemory("data/mapnametable.txt") -- Adjust path as needed (or extract from GRF)
+elseif mode == "file" then
+	source = source or "mapnametable.txt"
+	mapNameTable = C_FileSystem.ReadFile(path.join("Exports", source))
+else
+	error("Inavalid mode (must be either 'grf' or 'file')")
+end
+
+C_FileSystem.WriteFile(path.join("Exports", "mapnametable.txt"), mapNameTable)
 
 local mapNames = {}
 
-local lines = string_explode(mapNameTable, "\n")
-for index, line in ipairs(lines) do
+local lines = string_explode(mapNameTable, "\r\n")
+
+local function processLine(line)
+	-- Drop comments and empty lines
+	if line == "" or line:sub(1, 2) == "//" then
+		return
+	end
+
 	local tokens = string_explode(line, "#") -- Assumes table is already cleaned-up
 	local mapID = path.basename(tokens[1], ".rsw") -- Strip file extension
 	local displayName = string_gsub(tokens[2], "%s$", "") -- Strip trailing whitespace
@@ -16,10 +38,26 @@ for index, line in ipairs(lines) do
 	mapNames[mapID] = displayName
 end
 
-mapNames.login_screen = "Login Screen" -- Entry point (arbitrary)
+for index, line in ipairs(lines) do
+	processLine(line)
+end
 
-local outputFilePath = "DB/map-display-names.json"
-printf("Saving JSON-encoded display names to %s", outputFilePath)
+local existingMapDB = require("DB.Maps")
 
-local jsonString = json.prettier(mapNames)
-C_FileSystem.WriteFile(outputFilePath, jsonString)
+for mapID, mapInfo in pairs(existingMapDB) do
+	-- Discard duplicates (keep the existing data as a priority as it may have been modified)
+	mapNames[mapID] = mapInfo.displayName
+end
+
+local mapDB = {}
+for mapID, displayName in pairs(mapNames) do
+	mapDB[mapID] = {
+		displayName = displayName,
+	}
+end
+
+local outputFilePath = "DB/Maps.lua"
+local luaImportString = "return \n" .. dump(mapDB)
+
+printf("Saving %d map display names to %s", table.count(mapDB), outputFilePath)
+C_FileSystem.WriteFile(outputFilePath, luaImportString)
