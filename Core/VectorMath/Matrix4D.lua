e157a8d3005bc5c4e099da2d5331a81effe16e55
RDW <duckwhale@mail.ru>
2023-12-26 14:40:52 +0100

    Core: Add a SetColum utility method to the Matrix4D metatype
    It's a bit meh, but I suppose one can easily get confused with indices here.

--- Core/VectorMath/Matrix4D.lua
+++ Core/VectorMath/Matrix4D.lua
@@ -66,6 +66,20 @@ function Matrix4D:CreateIdentity()
 	return identityMatrix
 end
 
+function Matrix4D:SetColumn(columnIndex, firstValue, secondValue, thirdValue, fourthValue)
+	if columnIndex == 1 then
+		self.x1, self.y1, self.z1, self.w1 = firstValue, secondValue, thirdValue, fourthValue
+	elseif columnIndex == 2 then
+		self.x2, self.y2, self.z2, self.w2 = firstValue, secondValue, thirdValue, fourthValue
+	elseif columnIndex == 3 then
+		self.x3, self.y3, self.z3, self.w3 = firstValue, secondValue, thirdValue, fourthValue
+	elseif columnIndex == 4 then
+		self.x4, self.y4, self.z4, self.w4 = firstValue, secondValue, thirdValue, fourthValue
+	else
+		error(format("Invalid column index %s (valid indices are one through four)", columnIndex), 0)
+	end
+end
+
 Matrix4D.__index = Matrix4D
 
 return ffi.metatype("Matrix4D", Matrix4D)
