116ada840d4b09bbab589dc8ed827eec56fb6457
RDW <duckwhale@mail.ru>
2024-01-07 17:22:50 +0100

    Update export-grf-names.lua

--- Tools/export-grf-names.lua
+++ Tools/export-grf-names.lua
@@ -1,6 +1,7 @@
 local RagnarokGRF = require("Core.FileFormats.RagnarokGRF")
 
 local iconv = require("iconv")
+local json = require("json")
 
 local grfPath = arg[1] or "data.grf"
 
@@ -20,13 +21,17 @@ table.sort(originalFileNames)
 local englishFileNames = {}
 local koreanFileNames = {}
 
+-- The heuristic is a little sketchy, but it should work in this context
+local function IsEnglishPhrase(phrase)
+	local asciiPhrase = iconv.convert(phrase, "UTF-8", "CP949")
+	local isEnglishPhrase = (phrase == asciiPhrase)
+	return isEnglishPhrase
+end
+
 printf("Original file names: %d", #originalFileNames)
 
 for index, fileName in ipairs(originalFileNames) do
-	-- The heuristic is a little sketchy, but it should work in this context
-	local asciiFileName = iconv.convert(fileName, "UTF-8", "CP949")
-	local isEnglishFileName = (fileName == asciiFileName)
-	if not isEnglishFileName then
+	if not IsEnglishPhrase(fileName) then
 		table.insert(koreanFileNames, fileName)
 	else
 		table.insert(englishFileNames, fileName)
@@ -37,3 +42,31 @@ printf("English file names: %d", #englishFileNames)
 printf("Korean file names: %d", #koreanFileNames)
 
 assert(#englishFileNames + #koreanFileNames == #originalFileNames)
+
+local koreanWordsOrPhrases = {}
+
+for index, fileName in ipairs(koreanFileNames) do
+	local tokens = string.explode(fileName, "/") -- Paths are normalized so this is fine
+	-- print(fileName, #tokens)
+	for _, token in ipairs(tokens) do
+		-- print(_, token)
+		if not IsEnglishPhrase(token) then
+			-- print(token)
+			-- TODO strip file extension (don't need to translate it)
+			token = path.basename(token)
+			-- table.insert(koreanWordsOrPhrases, token)
+			koreanWordsOrPhrases[token] = token
+		end
+	end
+end
+
+-- TODO sort, filter out duplicates
+-- dump(koreanWordsOrPhrases)
+
+printf("Number of Korean phrases or tokens: %d", table.count(koreanWordsOrPhrases))
+
+local localizationTable = json.prettier(koreanWordsOrPhrases)
+local outputFilePath = path.join("Exports", "grf-names-kr.json")
+printf("Storing localization table: %s", outputFilePath)
+
+C_FileSystem.WriteFile(outputFilePath, localizationTable)
