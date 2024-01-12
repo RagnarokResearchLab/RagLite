cf8f0d7fcbca9db3d9e2524616a0a0efb3d61df7
RDW <duckwhale@mail.ru>
2024-01-12 19:24:29 +0100

    Tests: Improve the output of the shader validation script
    Matter of taste, eh?

--- validate-shaders.sh
+++ validate-shaders.sh
@@ -1,5 +1,5 @@
 set -e
 
-SHADERS_DIR=$(pwd)/Core/NativeClient/Shaders
+SHADERS_DIR=Core/NativeClient/Shaders
 
-find $SHADERS_DIR -type f -exec bash -c 'echo "Validating shader: {}"; naga {}' \; 
+find $SHADERS_DIR -type f -exec bash -c 'echo "Validating shader source: {}"; echo [naga] $(naga {})' \; 
