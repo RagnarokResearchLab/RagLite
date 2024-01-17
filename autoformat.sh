8b4982d156da5370276c628da77686cef101f130
RDW <duckwhale@mail.ru>
2024-01-17 03:33:44 +0100

    Repo: Add an optional --quick flag to the autoformat script
    Some of the database files are huge, so formatting takes a lot of time and hogs memory.
    
    Since they're rarely changed, can skip them for local development (but never in CI runs).

--- autoformat.sh
+++ autoformat.sh
@@ -1,1 +1,7 @@
-stylua . --verbose
+if [ "$1" = "--quick" ]; then
+	# Some of the database files are huge, so formatting takes a lot of time and hogs memory
+	# Since they're rarely changed, can skip them for local development (but never in CI runs)
+    stylua . --verbose --glob '*.lua' --glob '!DB/*'
+else
+    stylua . --verbose
+fi
