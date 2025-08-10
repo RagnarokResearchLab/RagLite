739a76bf3e1d077f5d67d05e21f59ea7ffb181ab
RDW <duckwhale@mail.ru>
2025-08-10 11:08:28 +0200

    Client: Outline the platform detection via defines
    Exciting stuff.

--- Core/RagLite2.cpp
+++ Core/RagLite2.cpp
@@ -1,3 +1,19 @@
+#pragma once
+
+#ifdef _WIN32
+#define RAGLITE_PLATFORM_WINDOWS
+#elifdef __APPLE__
+#define RAGLITE_PLATFORM_MACOS
+#elifdef __linux__
+#define RAGLITE_PLATFORM_LINUX
+#else
+#define RAGLITE_UNSUPPORTED_PLATFORM
+#endif
+
+#ifdef RAGLITE_UNSUPPORTED_PLATFORM
+#error "Unsupported Platform: OS-specific code paths have yet to be ported"
+#endif
+
 #define VC_EXTRALEAN
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
