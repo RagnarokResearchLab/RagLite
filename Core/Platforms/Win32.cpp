3e453440a0fa066b71511793723cdb3865ea1cbe
RDW <duckwhale@mail.ru>
2025-08-10 23:36:36 +0200

    Client: Improve the GDI debug drawing functionality

--- Core/Platforms/Win32.cpp
+++ Core/Platforms/Win32.cpp
@@ -2,45 +2,250 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-#define GLOBAL static;
-#define INTERNAL static;
+#define GLOBAL static
+#define INTERNAL static
+
 #define TODO(msg) OutputDebugStringA(msg);
+
+struct BitmapDimensions {
+  int width;
+  int height;
+};
 
 GLOBAL BITMAPINFO bitmapInfo;
 GLOBAL void *bitmapBuffer;
-GLOBAL HBITMAP bitmap;
-GLOBAL HDC bitmapDeviceContext;
+GLOBAL BitmapDimensions bitmapSize;
+GLOBAL int bitmapColorDepthBytesPerPixel = 4;
 
 GLOBAL bool APPLICATION_SHOULD_EXIT = false;
-GLOBAL const char *WINDOW_TITLE = "RagLite2 - Win32/GDI";
+GLOBAL const char *WINDOW_TITLE = "RagLite2";
 
+enum gdi_debug_pattern {
+  PATTERN_SHIFTING_GRADIENT,
+  PATTERN_CIRCULAR_RIPPLE,
+  PATTERN_CHECKERBOARD,
+  PATTERN_AXIS_GRADIENTS,
+  PATTERN_GRID_SCANLINE,
+  PATTERN_COUNT
+};
+GLOBAL gdi_debug_pattern GDI_DEBUG_PATTERN = PATTERN_SHIFTING_GRADIENT;
+
+void DebugDraw_UpdatePattern() {
+  DWORD MS_PER_SECOND = 1000;
+
+  DWORD ticks = GetTickCount();
+  DWORD seconds = ticks / MS_PER_SECOND;
+  DWORD updateInterval = 5;
+
+  GDI_DEBUG_PATTERN =
+      (gdi_debug_pattern)((seconds / updateInterval) % PATTERN_COUNT);
+}
+
+INTERNAL void DebugDraw_ShiftingGradient(int offsetBlue, int offsetGreen) {
+  if (!bitmapBuffer)
+    return;
+
+  int stride = bitmapSize.width * bitmapColorDepthBytesPerPixel;
+  uint8 *row = (uint8 *)bitmapBuffer;
+  for (int y = 0; y < bitmapSize.height; ++y) {
+    uint32 *pixel = (uint32 *)row;
+    for (int x = 0; x < bitmapSize.width; ++x) {
+      uint8 blue = (x + offsetBlue) & 0xFF;
+      uint8 green = (y + offsetGreen) & 0xFF;
+
+      *pixel++ = ((green << 8) | blue);
+    }
+
+    row += stride;
+  }
+}
+
+// TODO Eliminate this
+#include <math.h>
+
+INTERNAL void DebugDraw_CircularRipple(int time, int dummy) {
+  if (!bitmapBuffer)
+    return;
+
+  int stride = bitmapSize.width * bitmapColorDepthBytesPerPixel;
+  uint8 *row = (uint8 *)bitmapBuffer;
+
+  for (int y = 0; y < bitmapSize.height; ++y) {
+    uint32 *pixel = (uint32 *)row;
+    for (int x = 0; x < bitmapSize.width; ++x) {
+
+      int centerX = bitmapSize.width / 2;
+      int centerY = bitmapSize.height / 2;
+      float dx = (float)(x - centerX);
+      float dy = (float)(y - centerY);
+      float dist = sqrtf(dx * dx + dy * dy);
+      float wave = 0.5f + 0.5f * sinf(dist / 5.0f - time * 0.1f);
+
+      uint8 blue = (uint8)(wave * 255);
+      uint8 green = (uint8)((1.0f - wave) * 255);
+      uint8 red = (uint8)((0.5f + 0.5f * sinf(time * 0.05f)) * 255);
+
+      *pixel++ = (red << 16) | (green << 8) | blue;
+    }
+    row += stride;
+  }
+}
+
+INTERNAL void DebugDraw_Checkerboard(int time, int unused) {
+  if (!bitmapBuffer)
+    return;
+
+  int stride = bitmapSize.width * bitmapColorDepthBytesPerPixel;
+  uint8 *row = (uint8 *)bitmapBuffer;
+
+  float angle = time * 0.02f;
+  float cosA = cosf(angle);
+  float sinA = sinf(angle);
+
+  int cx = bitmapSize.width / 2;
+  int cy = bitmapSize.height / 2;
+
+  int squareSize = 32;
+
+  for (int y = 0; y < bitmapSize.height; ++y) {
+    uint32 *pixel = (uint32 *)row;
+    for (int x = 0; x < bitmapSize.width; ++x) {
+      int rx = x - cx;
+      int ry = y - cy;
+
+      float rX = rx * cosA - ry * sinA;
+      float rY = rx * sinA + ry * cosA;
+
+      int checkerX = ((int)floorf(rX / squareSize)) & 1;
+      int checkerY = ((int)floorf(rY / squareSize)) & 1;
+
+      uint8 c = (checkerX ^ checkerY) ? 200 : 80;
+      *pixel++ = (c << 16) | (c << 8) | c;
+    }
+    row += stride;
+  }
+}
+
+INTERNAL void DebugDraw_AxisGradients(int time, int unused) {
+  if (!bitmapBuffer)
+    return;
+
+  int stride = bitmapSize.width * bitmapColorDepthBytesPerPixel;
+  uint8 *row = (uint8 *)bitmapBuffer;
+
+  int cx = bitmapSize.width / 2;
+  int cy = bitmapSize.height / 2;
+
+  for (int y = 0; y < bitmapSize.height; ++y) {
+    uint32 *pixel = (uint32 *)row;
+    for (int x = 0; x < bitmapSize.width; ++x) {
+      uint8 red = (uint8)((x * 255) / bitmapSize.width);
+      uint8 green = (uint8)((y * 255) / bitmapSize.height);
+      uint8 blue = 0;
+
+      if (x == cx || y == cy) {
+        red = green = blue = 255;
+      }
+
+      *pixel++ = (red << 16) | (green << 8) | blue;
+    }
+    row += stride;
+  }
+}
+
+INTERNAL void DebugDraw_GridScanline(int time, int unused) {
+  if (!bitmapBuffer)
+    return;
+
+  int stride = bitmapSize.width * bitmapColorDepthBytesPerPixel;
+  uint8 *row = (uint8 *)bitmapBuffer;
+
+  int gridSpacing = 32;
+  int scanY = (time / 2) % bitmapSize.height;
+
+  for (int y = 0; y < bitmapSize.height; ++y) {
+    uint32 *pixel = (uint32 *)row;
+    for (int x = 0; x < bitmapSize.width; ++x) {
+      uint8 c = 180;
+
+      if (x % gridSpacing == 0 || y % gridSpacing == 0)
+        c = 100;
+
+      if (y == scanY)
+        c = 255;
+
+      *pixel++ = (c << 16) | (c << 8) | c;
+    }
+    row += stride;
+  }
+}
+
+INTERNAL void DebugDraw_WriteBitmap(int paramA, int paramB) {
+  switch (GDI_DEBUG_PATTERN) {
+  case PATTERN_SHIFTING_GRADIENT:
+    DebugDraw_ShiftingGradient(paramA, paramB);
+    break;
+  case PATTERN_CIRCULAR_RIPPLE:
+    DebugDraw_CircularRipple(paramA, paramB);
+    break;
+  case PATTERN_CHECKERBOARD:
+    DebugDraw_Checkerboard(paramA, paramB);
+    break;
+  case PATTERN_AXIS_GRADIENTS:
+    DebugDraw_AxisGradients(paramA, paramB);
+    break;
+  case PATTERN_GRID_SCANLINE:
+    DebugDraw_GridScanline(paramA, paramB);
+    break;
+  }
+}
+
+// TODO Might want to fix the stretching/aspect ratio bugs
 INTERNAL void ResizeBackBuffer(int width, int height) {
-  if (bitmap) {
-    DeleteObject(bitmap);
+  if (bitmapBuffer) {
+    VirtualFree(bitmapBuffer, 0, MEM_RELEASE);
   }
 
-  if (!bitmapDeviceContext) {
-    bitmapDeviceContext = CreateCompatibleDC(0);
-  }
+  bitmapSize.width = width;
+  bitmapSize.height = height;
 
   bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo.bmiHeader);
-  bitmapInfo.bmiHeader.biWidth = width;
-  bitmapInfo.bmiHeader.biHeight = height;
+  bitmapInfo.bmiHeader.biWidth = bitmapSize.width;
+  bitmapInfo.bmiHeader.biHeight = -bitmapSize.height; // Inverted y
   bitmapInfo.bmiHeader.biPlanes = 1;
   bitmapInfo.bmiHeader.biBitCount = 32;
-  bitmapInfo.bmiHeader.biCompression = BI_RGB;
+  bitmapInfo.bmiHeader.biCompression = BI_RGB; // Alpha is unused
 
-  bitmap = CreateDIBSection(bitmapDeviceContext, &bitmapInfo, DIB_RGB_COLORS,
-                            &bitmapBuffer, 0, 0);
+  int bitmapBufferLength = (width * height) * bitmapColorDepthBytesPerPixel;
+
+  TODO("VirtualAlloc outside global arena\n")
+  bitmapBuffer = VirtualAlloc(0, bitmapBufferLength, MEM_COMMIT | MEM_RESERVE,
+                              PAGE_READWRITE);
+  // TODO Reset to clear color here?
 }
 
-INTERNAL void OnUpdate(HDC displayDeviceContext, int x, int y, int width,
-                       int height) {
-  StretchDIBits(displayDeviceContext, x, y, width, height, x, y, width, height,
-                bitmapBuffer, &bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+INTERNAL void OnUpdate(HDC displayDeviceContext, RECT *clientRect, int x, int y,
+                       int width, int height) {
+  int xDest = 0;
+  int yDest = 0;
+  int xSrc = 0;
+  int ySrc = 0;
+
+  int destWidth = bitmapSize.width;
+  int destHeight = bitmapSize.height;
+
+  int windowWidth = clientRect->right - clientRect->left;
+  int windowHeight = clientRect->bottom - clientRect->top;
+  int srcWidth = windowWidth;
+  int srcHeight = windowHeight;
+
+  StretchDIBits(displayDeviceContext, xDest, yDest, destWidth, destHeight, xSrc,
+                ySrc, srcWidth, srcHeight, bitmapBuffer, &bitmapInfo,
+                DIB_RGB_COLORS, SRCCOPY);
 }
 
-LRESULT CALLBACK OnMessage(HWND window, auto message, auto argW, auto argL) {
+LRESULT CALLBACK OnMessage(HWND window, UINT message, WPARAM argW,
+                           LPARAM argL) {
   LRESULT result = 0;
 
   switch (message) {
@@ -72,7 +277,9 @@ LRESULT CALLBACK OnMessage(HWND window, auto message, auto argW, auto argL) {
     int y = paintInfo.rcPaint.top;
     int width = paintInfo.rcPaint.right - paintInfo.rcPaint.left;
     int height = paintInfo.rcPaint.bottom - paintInfo.rcPaint.top;
-    OnUpdate(displayDeviceContext, x, y, width, height);
+    RECT clientRect;
+    GetClientRect(window, &clientRect);
+    OnUpdate(displayDeviceContext, &clientRect, x, y, width, height);
     EndPaint(window, &paintInfo);
   } break;
 
@@ -107,22 +314,37 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE unused, LPSTR commandLine,
         WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
         CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, instance, 0);
     if (mainWindow) {
+      RECT clientRect;
+      GetClientRect(mainWindow, &clientRect);
+      ResizeBackBuffer(clientRect.right - clientRect.left,
+                       clientRect.bottom - clientRect.top);
+
       MSG message;
-      constexpr HWND WINDOW_AND_THREAD = NULL;
-      constexpr auto MINMAX_FILTER_NONE = NULL;
+      int offsetX = 0;
+      int offsetY = 0;
       while (!APPLICATION_SHOULD_EXIT) {
-        BOOL result = GetMessageA(&message, WINDOW_AND_THREAD,
-                                  MINMAX_FILTER_NONE, MINMAX_FILTER_NONE);
-        if (result > 0) {
+        while (PeekMessage(&message, 0, 0, 0, PM_REMOVE)) {
           TranslateMessage(&message);
           DispatchMessageA(&message);
-        } else if (result < 0) {
-          TODO("Unexpected error in main loop - Exiting...");
-          break;
-        } else {
-          TODO("Received WM_QUIT - Exiting...");
-          break;
+          if (message.message == WM_QUIT)
+            APPLICATION_SHOULD_EXIT = true;
         }
+        DebugDraw_UpdatePattern();
+        DebugDraw_WriteBitmap(offsetX, offsetY);
+
+        // TODO Consider CS_OWNC? Might be faster, but there are footguns...
+        // See https://devblogs.microsoft.com/oldnewthing/20060601-06/?p=31003
+        HDC displayDeviceContext = GetDC(mainWindow);
+        RECT clientRect;
+        GetClientRect(mainWindow, &clientRect);
+        int windowWidth = clientRect.right - clientRect.left;
+        int windowHeight = clientRect.bottom - clientRect.top;
+        OnUpdate(displayDeviceContext, &clientRect, 0, 0, windowWidth,
+                 windowHeight);
+        ReleaseDC(mainWindow, displayDeviceContext);
+
+        ++offsetX;
+        offsetY += 2;
       }
     } else {
       TODO("Failed to CreateWindowExA - Exiting...");
