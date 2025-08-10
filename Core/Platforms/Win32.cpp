2a3e208c469c871623456a26af1b9591ad28ed8b
RDW <duckwhale@mail.ru>
2025-08-10 16:15:43 +0200

    Client: Replace GDI clear color with a DIB backing store

--- Core/Platforms/Win32.cpp
+++ Core/Platforms/Win32.cpp
@@ -3,17 +3,53 @@
 #include <windows.h>
 
 #define GLOBAL static;
+#define INTERNAL static;
 #define TODO(msg) OutputDebugStringA(msg);
+
+GLOBAL BITMAPINFO bitmapInfo;
+GLOBAL void *bitmapBuffer;
+GLOBAL HBITMAP bitmap;
+GLOBAL HDC bitmapDeviceContext;
 
 GLOBAL bool APPLICATION_SHOULD_EXIT = false;
 GLOBAL const char *WINDOW_TITLE = "RagLite2 - Win32/GDI";
+
+INTERNAL void ResizeBackBuffer(int width, int height) {
+  if (bitmap) {
+    DeleteObject(bitmap);
+  }
+
+  if (!bitmapDeviceContext) {
+    bitmapDeviceContext = CreateCompatibleDC(0);
+  }
+
+  bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo.bmiHeader);
+  bitmapInfo.bmiHeader.biWidth = width;
+  bitmapInfo.bmiHeader.biHeight = height;
+  bitmapInfo.bmiHeader.biPlanes = 1;
+  bitmapInfo.bmiHeader.biBitCount = 32;
+  bitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+  bitmap = CreateDIBSection(bitmapDeviceContext, &bitmapInfo, DIB_RGB_COLORS,
+                            &bitmapBuffer, 0, 0);
+}
+
+INTERNAL void OnUpdate(HDC displayDeviceContext, int x, int y, int width,
+                       int height) {
+  StretchDIBits(displayDeviceContext, x, y, width, height, x, y, width, height,
+                bitmapBuffer, &bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+}
 
 LRESULT CALLBACK OnMessage(HWND window, auto message, auto argW, auto argL) {
   LRESULT result = 0;
 
   switch (message) {
   case WM_SIZE: {
-    TODO("Received WM_SIZE\n");
+    RECT clientRec;
+    GetClientRect(window, &clientRec);
+    int width = clientRec.right - clientRec.left;
+    int height = clientRec.bottom - clientRec.top;
+    ResizeBackBuffer(width, height);
   } break;
 
   case WM_CLOSE: {
@@ -36,15 +72,7 @@ LRESULT CALLBACK OnMessage(HWND window, auto message, auto argW, auto argL) {
     int y = paintInfo.rcPaint.top;
     int width = paintInfo.rcPaint.right - paintInfo.rcPaint.left;
     int height = paintInfo.rcPaint.bottom - paintInfo.rcPaint.top;
-
-    static DWORD rasterOpCode = WHITENESS;
-    PatBlt(displayDeviceContext, x, y, width, height, rasterOpCode);
-
-    if (rasterOpCode == WHITENESS)
-      rasterOpCode = BLACKNESS;
-    else
-      rasterOpCode = WHITENESS;
-
+    OnUpdate(displayDeviceContext, x, y, width, height);
     EndPaint(window, &paintInfo);
   } break;
 
