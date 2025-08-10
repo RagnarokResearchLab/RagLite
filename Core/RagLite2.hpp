7a9d912303906035d556c07e966365c2d56370b1
RDW <duckwhale@mail.ru>
2025-08-10 11:23:48 +0200

    Client: Introduce basic sanity checks for x64 platforms

--- Core/RagLite2.hpp
+++ Core/RagLite2.hpp
@@ -1,15 +1,21 @@
 #pragma once
 
 #ifdef _WIN32
-#define RAGLITE_PLATFORM_WINDOWS
+	#define RAGLITE_PLATFORM_WINDOWS
 #elifdef __APPLE__
-#define RAGLITE_PLATFORM_MACOS
+	#define RAGLITE_PLATFORM_MACOS
 #elifdef __linux__
-#define RAGLITE_PLATFORM_LINUX
+	#define RAGLITE_PLATFORM_LINUX
 #else
-#define RAGLITE_UNSUPPORTED_PLATFORM
+	#define RAGLITE_UNSUPPORTED_PLATFORM
 #endif
 
 #ifdef RAGLITE_UNSUPPORTED_PLATFORM
-#error "Unsupported Platform: OS-specific code paths have yet to be ported"
+	#error "Unsupported Platform: OS-specific code paths have yet to be ported"
+#endif
+
+static_assert(sizeof(void*) == 8, "Only 64-bit platforms are currently supported");
+
+#if defined(__BYTE_ORDER__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    #error "Only Little-Endian platforms are currently supported"
 #endif
