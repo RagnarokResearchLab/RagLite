cf2e66f9325647f3ef5a9f0ca190bc4946de02c2
RDW <rdw-software@proton.me>
2025-08-17 23:50:06 +0200

    Update RagLite2.cpp

--- Core/RagLite2.cpp
+++ Core/RagLite2.cpp
@@ -45,35 +45,7 @@ inline int Percent(double percentage) {
 
 GLOBAL float TARGET_FPS = 120;
 
-// Intrinsicts (TODO Move elsewhere later)
-#include <intrin.h>
-#include <memory.h>
-
-// TODO Does that work on GCC? Move to platform defines.
-// TODO intrinsics -> not here
-GLOBAL int CPU_INFO_MASK[4] = {};
-GLOBAL char CPU_BRAND_STRING[0x40] = { "N/A (__cpuid intrinsic not yet supported)" };
-
-#ifdef RAGLITE_COMPILER_MSVC
-INTERNAL void IntrinsicsReadCPUID() {
-
-	__cpuid(CPU_INFO_MASK, 0x80000000);
-	unsigned int nExIds = CPU_INFO_MASK[0];
-
-	if(nExIds >= 0x80000004) {
-		__cpuid((int*)CPU_INFO_MASK, 0x80000002);
-		memcpy(CPU_BRAND_STRING, CPU_INFO_MASK, sizeof(CPU_INFO_MASK));
-
-		__cpuid((int*)CPU_INFO_MASK, 0x80000003);
-		memcpy(CPU_BRAND_STRING + 16, CPU_INFO_MASK, sizeof(CPU_INFO_MASK));
-
-		__cpuid((int*)CPU_INFO_MASK, 0x80000004);
-		memcpy(CPU_BRAND_STRING + 32, CPU_INFO_MASK, sizeof(CPU_INFO_MASK));
-	}
-#else
-
-#endif
-}
+#include "Intrinsics.hpp"
 
 // String utilities (TODO Move elsewhere later)
 INTERNAL int FloatToString(char* buffer, float value, int decimals) {
