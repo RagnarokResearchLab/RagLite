b8f168e18e46c81e7a35e658e2e57b422bf35aab
RDW <rdw-software@proton.me>
2025-08-17 16:59:04 +0200

    Update DebugDraw.cpp

--- Core/Platforms/Win32/DebugDraw.cpp
+++ Core/Platforms/Win32/DebugDraw.cpp
@@ -45,7 +45,7 @@ constexpr COLORREF RGB_COLOR_RED = RGB(200, 0, 0);
 constexpr COLORREF RGB_COLOR_YELLOW = RGB(200, 200, 0);
 constexpr COLORREF RGB_COLOR_WHITE = RGB(200, 200, 200);
 
-constexpr COLORREF UI_BACKGROUND_COLOR = RGB_COLOR_DARK;
+constexpr COLORREF UI_PANEL_COLOR = RGB_COLOR_DARK;
 constexpr COLORREF UI_TEXT_COLOR = RGB_COLOR_WHITE;
 
 COLORREF GetUsageColor(int percent) {
@@ -102,7 +102,7 @@ void DebugDrawMemoryUsageOverlay(gdi_surface_t& surface) {
 		startX + 1024,
 		startY + (DEBUG_OVERLAY_LINE_HEIGHT * NUM_SECTIONS * Y_PER_SECTION)
 	};
-	HBRUSH panelBrush = CreateSolidBrush(UI_BACKGROUND_COLOR);
+	HBRUSH panelBrush = CreateSolidBrush(UI_PANEL_COLOR);
 	FillRect(offscreenDeviceContext, &backgroundPanelRect, panelBrush);
 	DeleteObject(panelBrush);
 
@@ -306,7 +306,7 @@ void DebugDrawProcessorUsageOverlay(gdi_surface_t& surface) {
 		startX + 360,
 		startY + (DEBUG_OVERLAY_LINE_HEIGHT * 11)
 	};
-	HBRUSH panelBrush = CreateSolidBrush(UI_BACKGROUND_COLOR);
+	HBRUSH panelBrush = CreateSolidBrush(UI_PANEL_COLOR);
 	FillRect(displayDeviceContext, &panelRect, panelBrush);
 	DeleteObject(panelBrush);
 
