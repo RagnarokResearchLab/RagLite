e26f66e1a4e4970171af00b9fb4ee202a6a54d8d
RDW <rdw-software@proton.me>
2025-08-17 21:24:07 +0200

    Update RagLite2.cpp

--- Core/RagLite2.cpp
+++ Core/RagLite2.cpp
@@ -11,6 +11,22 @@ typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
 
+	inline uint32 Kilobytes(uint32 bytes) {
+		return bytes / 1024;
+	}
+
+	inline uint32 Megabytes(uint32 bytes) {
+		return Kilobytes(bytes) / 1024;
+	}
+
+		inline uint32 Gigabytes(uint32 bytes) {
+		return Megabytes(bytes) / 1024;
+	}
+
+		inline uint32 Terabytes(uint32 bytes) {
+		return Gigabytes(bytes) / 1024;
+	}
+
 #ifdef RAGLITE_PLATFORM_WINDOWS
 #include "Platforms/Win32.cpp"
 #endif
