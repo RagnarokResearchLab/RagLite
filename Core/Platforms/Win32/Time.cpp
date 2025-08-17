dd402ce2c01885112e934de2c6a6501d0dc472f2
RDW <rdw-software@proton.me>
2025-08-17 14:17:44 +0200

    Update Time.cpp

--- Core/Platforms/Win32/Time.cpp
+++ Core/Platforms/Win32/Time.cpp
@@ -1,5 +1,17 @@
-double GetProcessorUsage() {
-	static FILETIME prevSysKernel, prevSysUser;
+typedef struct cpu_performance_metrics {
+	bool isInitialized; // TODO count samples?
+	FILETIME previousSystemTimesIdle;
+	FILETIME previousSystemTimesKernel;
+	FILETIME previousSystemTimesUser;
+	FILETIME processCreationTime;
+	FILETIME processExitTime; // TBD err, what?
+	FILETIME processKernelTime;
+	FILETIME processUserTime;
+	double cpuUsageLastFrame; // TODO update once, query afterwards
+} performance_metrics_t;
+
+double GetProcessorUsageAllCores() {
+	static FILETIME prevSysKernel, prevSysUser; // TODO store GLOBAL in perf struct (same as perf counter)
 	static FILETIME prevProcKernel, prevProcUser;
 	static bool firstCall = true;
 
@@ -38,11 +50,19 @@ double GetProcessorUsage() {
 	if(sysTotal == 0)
 		return 0.0;
 
-	SYSTEM_INFO sysInfo;
-	GetSystemInfo(&sysInfo);
 
-	double cpuUsage = (100.0 * (double)procTotal / (double)sysTotal) * sysInfo.dwNumberOfProcessors;
+	// denominator = (global_kernel_time - old_global_kernel_time) + (global_user_time - old_global_user_time)
+	// ((kernel_time - old_kernel_time) + (user_time - old_user_time)) / denominator * 100
+	double cpuUsage = (100.0 * (double)procTotal / (double)sysTotal);
 	constexpr double EPSILON = 0.001;
 	if(cpuUsage - 100.0 > EPSILON) return 100.0; // TODO clamp
 	return cpuUsage;
+}
+
+double GetProcessorUsageSingleCore() {
+	// TODO Calling this twice in the same frame needs to recompute (revisit later)
+	// It's not exactly an accurate interpolation, but alas...
+SYSTEM_INFO sysInfo;
+	GetSystemInfo(&sysInfo);
+ return GetProcessorUsageAllCores() * sysInfo.dwNumberOfProcessors;
 }
