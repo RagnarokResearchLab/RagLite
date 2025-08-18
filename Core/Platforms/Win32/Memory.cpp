d072e3e890cb12ab45f46346afa1365b8a8a9979
RDW <rdw-software@proton.me>
2025-08-18 04:04:43 +0200

    win32 initialize global and frame arenas (details TBD)

--- Core/Platforms/Win32/Memory.cpp
+++ Core/Platforms/Win32/Memory.cpp
@@ -20,3 +20,59 @@ GLOBAL memory_arena_t MAIN_MEMORY = {
 	.used = 64 * 1024 * 42 * 256,
 	.allocationCount = 42
 };
+
+GLOBAL memory_arena_t TRANSIENT_MEMORY = {
+	.name = "Preallocated (Transient Memory)",
+	.lifetime = "Frame (Scoped Arena)",
+	.baseAddress = (void*)0xDEADBEEFull,
+	.reservedSize = 64u * 1024 * 42 * 1024,
+	.committedSize = 64u * 1024 * 42 * 512,
+	.used = 64 * 1024 * 42 * 256,
+	.allocationCount = 42
+};
+
+void SystemMemoryInitializeArenas() {
+// TODO Pin base address only in debug mode
+#if 0
+    	LPVOID mainMemoryBaseAddress = (LPVOID)Terabytes(2);
+    	LPVOID transientMemoryBaseAddress = (LPVOID)Terabytes(2) + (LPVOID)Terabytes(2);
+#else
+	LPVOID mainMemoryBaseAddress = 0;
+	LPVOID transientMemoryBaseAddress = (LPVOID)Terabytes(2);
+#endif
+
+	// TODO Assert page size matches allocation granularity
+
+	MAIN_MEMORY = {
+		// .name = "Preallocated (Main Memory)",
+		.name = "SMOLL",
+		.lifetime = "Forever (Global Arena)",
+		// .baseAddress = (void*)0xDEADBEEFull,
+		.baseAddress = VirtualAlloc(mainMemoryBaseAddress, Gigabytes(1) + Megabytes(32), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE),
+		.reservedSize = Gigabytes(1) + Megabytes(32),
+		// .reservedSize = 64u * 1024 * 42 * 1024,
+		// .commitedSize = 0,
+		.committedSize = 64u * 1024 * 42 * 512,
+		.used = 0,
+		// .used = 64 * 1024 * 42 * 256,
+		.allocationCount = 0
+		// .allocationCount = 42
+	};
+
+	TRANSIENT_MEMORY = {
+		.name = "HUGE",
+		// .name = "Preallocated (Transient Memory)",
+		.lifetime = "Frame (Scoped Arena)",
+		// .baseAddress = (void*)0xDEADBEEFull,
+		.baseAddress = (uint8*)MAIN_MEMORY.baseAddress + MAIN_MEMORY.reservedSize,
+		// .reservedSize = 64u * 1024 * 42 * 1024,
+		.reservedSize = Gigabytes(1),
+		// .committedSize = 64u * 1024 * 42 * 512,
+		.committedSize = 0,
+		// .used = 64 * 1024 * 42 * 256,
+		.used = 0,
+		// .allocationCount = 42
+		.allocationCount = 0
+
+	};
+}
