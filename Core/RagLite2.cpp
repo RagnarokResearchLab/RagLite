1e51fb5cf8c51359eaeed28611b5a9fcaffcf4b2
RDW <rdw-software@proton.me>
2025-08-19 02:04:38 +0200

    Revert "Update RagLite2.cpp"
    This reverts commit 6fec095f2648bc8761228d5c5f05c6c4a955b51f.

--- Core/RagLite2.cpp
+++ Core/RagLite2.cpp
@@ -3,16 +3,6 @@
 #define GLOBAL static
 #define INTERNAL static
 
-// TODO eliminate this include?
-#include <cassert>
-
-// Silence [-Wunused-value] compiler warnings by adding (void)
-#ifdef NDEBUG
-#define ASSUME(condition, message) ((void)0)
-#else
-#define ASSUME(condition, failureMessage) assert((void(failureMessage), condition))
-#endif
-
 #include "Intrinsics.hpp"
 #include "Numbers.hpp"
 #include "Strings.hpp"
