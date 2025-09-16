22268b4cb2af1529e541256a5ecd8c4bfcaf6231
RDW <rdw-software@proton.me>
2025-09-16 13:43:25 +0200

    Cleanup: Remove unused arguments in autoformat.sh
    Copy/pasted from another repository - forgot to remove these.

--- autoformat.sh
+++ autoformat.sh
@@ -33,7 +33,7 @@ fi
 
 echo "Discovering C/C++ sources ..."
 
-RELEVANT_C_FILES_TO_FORMAT=$(find . -type f -name "*.c" -print -o -name "*.h" -print -o -path "*/deps" -prune -o -path "*/ninjabuild-*" -prune)
+RELEVANT_C_FILES_TO_FORMAT=$(find . -type f -name "*.c" -print -o -name "*.h" -print)
 
 if [ -n "$RELEVANT_C_FILES_TO_FORMAT" ]; then
 	echo "Discovered C sources:"
@@ -45,7 +45,7 @@ else
 	echo "NO relevant C sources found"
 fi
 
-RELEVANT_CPP_FILES_TO_FORMAT=$(find . -type f -name "*.cpp" -print -o -name "*.hpp" -print -o -path "*/deps" -prune -o -path "*/ninjabuild-*" -prune)
+RELEVANT_CPP_FILES_TO_FORMAT=$(find . -type f -name "*.cpp" -print -o -name "*.hpp" -print)
 echo "Discovered C++ sources:"
 echo $RELEVANT_CPP_FILES_TO_FORMAT
 
