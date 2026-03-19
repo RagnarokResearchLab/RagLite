3c546b7f301201cd80d7a430c830feceaf29718a
RDW <rdw-software@proton.me>
2026-03-19 06:48:13 +0100

    Cleanup: Remove the obsolete TODO placeholder define
    It was one of the first things added, but is now obsolete. There's better ways to encode this, anyway.

--- Core/Platforms/Win32/GamePad.cpp
+++ Core/Platforms/Win32/GamePad.cpp
@@ -8,10 +8,10 @@ INTERNAL void GamePadPollControllers(gamepad_state_t& controllerInputs) {
 
 	constexpr DWORD MAX_SUPPORTED_GAMEPAD_COUNT = XUSER_MAX_COUNT;
 	for(DWORD gamePadID = 0; gamePadID < MAX_SUPPORTED_GAMEPAD_COUNT; ++gamePadID) {
-		XINPUT_STATE gamePadButtonState;
-		if(XInputGetState(gamePadID, &gamePadButtonState) != ERROR_SUCCESS) {
-			TODO("XInput detected a GamePad but it wasn't usable?\n")
-		}
+		XINPUT_STATE gamePadButtonState = {};
+		DWORD result = XInputGetState(gamePadID, &gamePadButtonState);
+		ASSUME(result != ERROR_DEVICE_NOT_CONNECTED, "GamePad detected, but not connected (...how does that work?)");
+		ASSUME(result == ERROR_SUCCESS, "GamePad detected, but it wasn't usable - despite being connected (...why?)");
 
 		XINPUT_GAMEPAD* gamePad = &gamePadButtonState.Gamepad;
 		controllerInputs.stickX = gamePad->sThumbLX;
