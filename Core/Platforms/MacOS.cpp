11b44898693a1a58da2ce669eb1c9ec56567231d
RDW <rdw-software@proton.me>
2026-03-20 18:58:59 +0100

    Core: Add placeholder entry points for OSX and Linux

---
+++ Core/Platforms/MacOS.cpp
@@ -0,0 +1,3 @@
+INTERNAL void PlatformRuntimeMain() {
+	// NYI
+}
--- Core/RagLite2.cpp
+++ Core/RagLite2.cpp
@@ -11,6 +11,13 @@ GLOBAL memory_arena_t TRANSIENT_MEMORY = {};
 
 #ifdef RAGLITE_PLATFORM_WINDOWS
 #include "Platforms/Win32.cpp"
+#elifdef RAGLITE_PLATFORM_MACOS
+#include "Platforms/MacOS.cpp"
+#elifdef RAGLITE_PLATFORM_LINUX
+#include "Platforms/Linux.cpp"
+#endif
+
+#ifdef RAGLITE_PLATFORM_WINDOWS
 int APIENTRY WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 #else
 int main()
