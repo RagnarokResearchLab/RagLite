970890ddedf4bf47d7a3fae1295b32807d8f345a
RDW <rdw-software@proton.me>
2026-03-21 00:35:56 +0100

    WIP: C++ GRF decoder (needs miniz/stb, hangul table, platform APIs, tests)

--- Tools/RagnarokTools.cpp
+++ Tools/RagnarokTools.cpp
@@ -79,6 +79,101 @@ INTERNAL void ListFileContentsPAK(platform_handle_t input, platform_handle_t out
 	printf("NYI :(\n");
 }
 
+// TODO Move to RagnarokGRF
+
+// TODO make sure API is compatible with C (no handles/references, just buffers? Or at least wrap it/expose C API also)
+// INTERNAL void DecodeFileContentsRagnarokGRF(roff_context_t& self, platform_handle_t& fileHandle) {
+// 	// TBD: Probably faster to read both (save one round trip to the kernel...) - benchmark later?
+// 	size_t headerSize = sizeof(grf_header_t);
+// 	void* writeBuffer = arena_push(self.tempArena, headerSize, false, false);
+// 	PlatformReadFileContents(fileHandle, 0, headerSize, writeBuffer);
+
+// 	grf_header_t* header = (grf_header_t*)writeBuffer;
+// 	size_t fileTableStartOffset = headerSize + header->file_table_offset;
+
+// 	size_t tableHeaderSize = sizeof(grf_file_table_t);
+// 	writeBuffer = arena_push(self.tempArena, tableHeaderSize, false, false);
+// 	PlatformReadFileContents(fileHandle, fileTableStartOffset, tableHeaderSize, writeBuffer); // TBD Get/Put
+// 	grf_file_table_t* tableHeader = (grf_file_table_t*)writeBuffer;
+// 	// grf_header_t* compressedFileTable = (grf_file_table_t*)writeBuffer;
+
+// 	if(!PlatformNoFileErrors(fileHandle)) { // TBD PlatformCheckFileErrors
+// 		printf("Failed to decode file contents\n");
+// 		return;
+// 	}
+
+// 	// TBD size_t
+// 	uint32_t compressedSize = tableHeader->compressed_size;
+// 	uint32_t decompressedSize = tableHeader->decompressed_size;
+
+// 	uint8_t* compressedTable = (uint8_t*)arena_push(self.tempArena, compressedSize, false, false);
+
+// 	PlatformReadFileContents(
+// 		fileHandle,
+// 		fileTableStartOffset + tableHeaderSize, // skip the two uint32s
+// 		compressedSize,
+// 		compressedTable);
+// 	printf("Compressed size: %u\nDecompressed size: %u\n", compressedSize, decompressedSize);
+// 	printf("first bytes: %02X %02X %02X %02X %02X %02X\n",
+// 		compressedTable[0],
+// 		compressedTable[1],
+// 		compressedTable[2],
+// 		compressedTable[3],
+// 		compressedTable[4],
+// 		compressedTable[5]);
+
+// 	platform_handle_t debugDumpFile = PlatformOpenFileHandle("compressed-toc.bin");
+// 	if(!PlatformNoFileErrors(debugDumpFile)) {
+// 		printf("Failed to open compressed-toc.bin (platform-specific error)\n");
+// 		printf("Make sure the file exists in the current working directory and is readable by this process\n");
+// 		return;
+// 	}
+// 	// TODO close handles, or let OS handle it (heh)?
+// 	PlatformWriteFileContents(debugDumpFile, 0, compressedSize, compressedTable);
+// 	uint8_t* decompressedTable = (uint8_t*)arena_push(self.tempArena, decompressedSize, false, false);
+
+// 	size_t outSize = decompressedSize;
+
+// 	if(!DecompressZlibBuffer(
+// 		   compressedTable,
+// 		   compressedSize,
+// 		   decompressedTable,
+// 		   outSize)) {
+// 		printf("Failed to decompress GRF table\n");
+// 		// return;
+// 	}
+
+// 	platform_handle_t debugDumpFileDEC = PlatformOpenFileHandle("decompressed-toc.bin");
+// 	PlatformWriteFileContents(debugDumpFileDEC, 0, decompressedSize, decompressedTable);
+
+// 	uint8_t* cursor = decompressedTable;
+// 	for(uint8_t index = 0; index < header->file_count; index++) {
+// 		size_t pathSize = 0;
+// 		utf8_string_t filePath = {
+// 			.size = 0,
+// 			.bytes = cursor,
+// 		};
+// 		while(*(cursor++) != '\0') {
+// 			pathSize++;
+// 			// TODO path normalization, possibly PlatformNormalizeFileName here, too (needs unit tests/special cases...)
+// 		}
+// 		// TBD StringEnsureNullTermination -> not needed here, as long as TOC is kept in memory... readonly view
+// 		// pathSize++;
+
+// 		filePath.size = pathSize;
+// 		// TODO add timers to all of these steps so that it can be compared to the LuaJIT version
+// 		utf8_string_t normalizedFilePath = euc_kr_to_utf8(filePath.buffer, self.tempArena); // push string, size param...
+
+// 		// TODO path separators -- also handled by Platform layer?
+// 		utf8_string_t localizedPathName = LocalizePath(self.tempArena, normalizedFilePath);
+// 		printf("Decoded %zd bytes: %s (EUC-KR) -> %s (UTF-8) - Translated: ", pathSize, filePath.characters, normalizedFilePath.characters, localizedPathName.characters);
+// 		printf("\n");
+
+// 		grf_file_entry_t* entry = (grf_file_entry_t*)cursor;
+// 		cursor += sizeof(grf_file_entry_t);
+// 	}
+// }
+
 INTERNAL void ListFileContentsGRF(platform_handle_t input, platform_handle_t output) {
 	// TODO: Plug in the C++ prototype (requires miniz or stb for DEFLATE, Hangul decoding POC - finish that one first?)
 	printf("NYI :(\n");
